f7a06ea4c588465377f92500ace2eb4f33b6a445
alihanks <alihanks@gmail.com>
2016-04-10 12:09:17 -0700

    can now take csv inputs from multiple locations and graph data together

--- data_processing.js
+++ data_processing.js
@@ -1,5 +1,21 @@
-var data_input = [];
-var data_string_array = [];
+var data_string_map = new Map();
+var timeParMap = new Map();
+// timeParMap structure: key = selected time range, value = [absolute time range,max nentries,data compression factor]
+timeParMap.set('Hour',[3600*1000,13,1]);
+timeParMap.set('Day',[24*3600*1000,289,6]);
+timeParMap.set('Week',[7*24*3600*1000,2017,12]);
+timeParMap.set('Month',[30*24*3600*1000,8641,48]);
+timeParMap.set('Year',[365*24*3600*1000,105121,288]);
+timeParMap.set('All',[365*24*3600*10000,1000000,1]);
+
+var calibMap = new Map();
+// calibMap structure: key = unit, value = calibration
+calibMap.set('CPM',1.0);
+calibMap.set('mrem/hr',0.0036);
+calibMap.set('&microSv/hr',0.036);
+calibMap.set('air travel/hr',0.420168067*0.036);
+calibMap.set('cigarettes/hr',0.00833333335*0.036);
+calibMap.set('X-rays/hr',0.2*0.036);
 
 //var url = 'https://radwatch.berkeley.edu/sites/default/files/dosenet/pinewood.csv?'
 //+ Math.random().toString(36).replace(/[^a-z]+/g, ''); // To solve browser caching issue
@@ -35,101 +51,49 @@ function singleErrorPlotter(e) {
 }
 
 function get_time_pars(time,end_date,start_date,nentries) {
-  var sample_size = 1;
-  switch(time) {
-    case 'Hour':
-      start_date = new Date(end_date.getTime() + -1*3600*1000);
-      nentries = Math.min(nentries,13); // 12 5 minute intervals in last hour
-      sample_size = 1; // don't compress
-    break;
-    case 'Day':
-      start_date = new Date(end_date.getTime() + -1*24*3600*1000);
-      nentries = Math.min(nentries,289); // 288 5 minute intervals in last day
-      sample_size = 6; // compress to twice per hour
-    break;
-    case 'Week':
-      start_date = new Date(end_date.getTime() + -7*24*3600*1000);
-      nentries = Math.min(nentries,2017); // 2016 in last week
-      sample_size = 12; // compress to one every hrs
-    break;
-    case 'Month':
-      start_date = new Date(end_date.getTime() + -30*24*3600*1000);
-      nentries = Math.min(nentries,8641); // 8640 in last month (30 days)
-      sample_size = 48; // compress to every 4 hrs
-    break;
-    case 'Year':
-      start_date = new Date(end_date.getTime() + -365*24*3600*1000);
-      nentries = Math.min(nentries,105121); // 105120 in last year
-      sample_size = 288;//1008; // compress to once a day
-    break;
-    case 'All':
-      //start_date = new Date(end_date.getTime() - start_date.getTime());
-      sample_size = 1; // don't compress;
-    break;
-  }
-  return [start_date,nentries,sample_size];
+  start_date = new Date(end_date.getTime() - timeParMap.get(time)[0]);
+  nentries = Math.min(nentries,timeParMap.get(time)[1]);
+  return [start_date,nentries];
 }
 
-function get_calibration(dose) {
-  var scale = 1.0;
-  switch(dose) {
-    case 'CPM':
-      scale = 1.0;
-    break;
-    case 'USV':
-      scale = 0.036;
-    break;
-    case 'REM':
-      scale = 0.0036;
-    break;
-    case 'cigarette':
-      scale = 0.036*0.00833333335;
-    break;
-    case 'medical':
-      scale = 0.036*0.2;
-    break;
-    case 'plane':
-      scale = 0.036*0.420168067;
-    break;
-  }
-  return scale;
-}
-
-function set_time_array(start_date,end_date){
+function set_time_bins(start_date,end_date,bin_size){
   var time_array = [];
-  var this_date = new Date(start_date.getTime());
+  // fill array with bin center with start date as low edge of first bin
+  var this_date = new Date(start_date.getTime() + bin_size/2);
   while( this_date.getTime() < end_date.getTime() ) {
     time_array.push(this_date);
-    this_date = new Date(this_date.getTime() + 5*60*1000); // Add 5 minute increments from start
+    // high edge of bin becomes new low edge
+    this_date = new Date(this_date.getTime() + bin_size);
   }
   return time_array;
 }
 
 function findNearestDate(alist, date, delta) {
   var first = 0;
-  var last = len(alist)-1;
+  var last = alist.length-1;
   var found = false;
   var index = -1;
 
   while( first<=last && !found ) {
-      var midpoint = (first + last)/2;
-      if( abs(alist[midpoint].getTime()-date.getTime()) < delta ) {
-        index = midpoint;
-        found = true;
-      }
-      else {
-        if( date.getTime() < alist[midpoint].getTime() )
-          last = midpoint-1;
-        else
-          first = midpoint+1;
-      }
+    var midpoint = Math.floor((first + last)/2);
+    if( Math.abs(alist[midpoint].getTime()-date.getTime()) < delta ) {
+      index = midpoint;
+      found = true;
     }
+    else {
+      if( date.getTime() < alist[midpoint].getTime() )
+        last = midpoint-1;
+      else
+        first = midpoint+1;
+    }
+  }
 
   return index;
 }
 
 function process_csv(text,dose,time) {
   var raw_data = [];
+  var data_input = [];
   var lines = text.split("\n");
   var nentries = lines.length; // compare to full set possible for given time interval and keep smaller value
   var newest_data = lines[lines.length-2].split(",");
@@ -140,9 +104,6 @@ function process_csv(text,dose,time) {
   var time_pars = get_time_pars(time,end_date,start_date,nentries);
   start_date = time_pars[0];
   nentries = time_pars[1];
-  var sample_size = time_pars[2];
-
-  var scale = get_calibration(dose);
 
   for( var i = lines.length - nentries; i < lines.length; ++i ) {
     if( i < 1 ) { continue; } // skip first line(s) with meta-data
@@ -156,13 +117,17 @@ function process_csv(text,dose,time) {
     }
   }
 
-  return average_data(raw_data,sample_size,scale);
+  var sample_size = timeParMap.get(time)[2];
+  var scale = calibMap.get(dose);
+  data_input = average_data(raw_data,sample_size,scale);
+  return data_input;
 }
 
 function average_data(raw_data,sample_size,scale)
 {
   var averaged_data = [];
-  for(n=0; n < Math.floor(raw_data.length/sample_size); n++){
+  var npoints = Math.floor(raw_data.length/sample_size);
+  for(n=0; n < npoints; n++){
     sub_data = raw_data.slice(n*sample_size,(n+1)*sample_size);
     var average = 0;
     for(i=0;i<sub_data.length;i++)
@@ -180,74 +145,106 @@ function average_data(raw_data,sample_size,scale)
   return averaged_data;
 }
 
-function process_all_data(data_array,dose,time) {
-  var all_data = [];
-  var combined_data = [];
-  for (index = 0; index < data_array.length; index++) {
-    this_data = process_csv(data_array[index],dose,time);
-
-  }
-  for (index = 0; index < all_data[0].length; index++) {
-    combined_data.push(all_data[0])
-  }
+// Maps each time bin to list of stations with [cpm,error] for that bin
+function fill_binned_data(data_map,time_bins,bin_size) {
+  var time_map = new Map();
+  data_map.forEach( function(data, location, data_map ) {
+    for( var i=0; i<data.length; i++) {
+      // date for each entry in data array for each location is the first element for that entry
+      var this_date = data[i][0];
+      // time bins is list of bin centers
+      // each date should then be either half a bin before or after the bin center
+      var this_bin = findNearestDate(time_bins,this_date,bin_size/2);
+      if( this_bin < 0 ) {
+        //console.log('WARNING: no valid time bin for data at '+this_date);
+        continue;
+      }
+      var this_data = data[i][1]; // this is [cmp,error]
+      // map data to given location for next step (need to know which locations went in to each bin)
+      var this_data_map = new Map();
+      this_data_map.set(location,this_data);
+      if( time_map.has(this_bin) ) {
+        time_map.get(this_bin).push(this_data_map);
+      }
+      else {
+        var this_data_map_array = [];
+        this_data_map_array.push(this_data_map);
+        time_map.set(this_bin,this_data_map_array);
+      }
+    }
+  });
+  return time_map;
 }
 
-function plot_data(location,dose,time,div) {
-  var title_text = location;
-  var y_text = '&microSv/hr';
-  var data_label = '&microSv/hr';
-  switch(dose) {
-  	case 'CPM':
-  	  title_text = title_text + ' in CPM ';
-  	  y_text = 'CPM';
-  	  data_label = "CPM";
-  	break;
-  	case 'USV':
-  	  title_text = title_text + ' in &microSv/hr ';
-  	  y_text = '&microSv/hr';
-  	  data_label = "µSv/hr";
-  	break;
-  	case 'REM':
-  	  title_text = title_text + ' in mrem/hr ';
-  	  y_text = 'mrem/hr';
-  	  data_label = "mrem/hr";
-  	break;
-  	case 'cigarette':
-  	  title_text = title_text + ' in cigarettes/hr ';
-  	  y_text = 'cigarettes/hr';
-  	  data_label = "cigarettes/hr";
-  	break;
-  	case 'medical':
-  	  title_text = title_text + ' in xRays/hr ';
-  	  y_text = 'xRays/hr';
-  	  data_label = "xRays/hr";
-  	break;
-  	case 'plane':
-  	  title_text = title_text + ' in airplane travel per hour ';
-  	  y_text = 'air-travel/hr';
-  	  data_label = "air-travel/hr";
-  	break;
-  }
+function fill_data_input(time_bins,time_map,locations) {
+  var data_input = [];
+  for ( var bin=0; bin<time_bins.length; bin++ ) {
+    //There might not be data for every time bin...
+    if( !time_map.has(bin) ) {
+      console.log("WARNING: no data for this time bin!");
+      continue;
+    }
 
-  switch(time) {
-  	case 'Hour':
-  	  title_text = title_text + 'for the most recent ' + time;
-  	break;
-  	case 'Day':
-  	  title_text = title_text + 'for the most recent ' + time;
-  	break;
-  	case 'Week':
-  	  title_text = title_text + 'for the most recent ' + time;
-  	break;
-  	case 'Month':
-  	  title_text = title_text + 'for the most recent ' + time;
-  	break;
-  	case 'Year':
-  	  title_text = title_text + 'for the most recent ' + time;
-  	break;
-  	case 'All':
-  	  title_text = 'All data for ' + title_text;
+    var data_array = [];
+    data_array.push(time_bins[bin]);
+    for( var j=0; j<locations.length; j++ ) {
+      var has_location = false;
+      for( var i=0; i<time_map.get(bin).length; i++ ) {
+        // if this entry for this time bin has data for this location add it to the data array for this time bin
+        if( time_map.get(bin)[i].has(locations[j]) ) {
+          data_array.push(time_map.get(bin)[i].get(locations[j]));
+          has_location = true;
+        }
+      }
+      if( !has_location )
+        data_array.push(null); // if no data for this location give it a null entry
+    }
+    data_input.push(data_array);
   }
+  return data_input;
+}
+
+function process_all_data(csv_map,dose,time) {
+  // fill map with key = location, value = [time,[cpm,error]] array from csv file  
+  // Full map of all data for all locations
+  var data_map = new Map();
+  // keep track of range of start and end times from all locations
+  var start_date_range = [];
+  var end_date_range = [];
+
+  length = csv_map.size;
+
+  csv_map.forEach( function(csv, location, csv_map ) {
+    //console.log('processing data for '+location+' with data length = '+csv.length);
+    var this_data = process_csv(csv,dose,time);
+    start_date_range.push(this_data[0][0]);
+    end_date_range.push(this_data[this_data.length-1][0]);
+    data_map.set(location,this_data);
+  });
+
+  // Set time binning based on full range of dates from all data
+  var start_date = new Date(Math.max.apply(null,start_date_range));
+  var end_date = new Date(Math.max.apply(null,end_date_range));
+  var sample_size = timeParMap.get(time)[2];
+  // default bin size: add 5 minute increments from start (5*60*1000)
+  // rebin by sample_size -> bin_size = default*sample_size
+  var bin_size = sample_size*5*60*1000;
+  var time_bins = set_time_bins(start_date,end_date,bin_size);
+  console.log([time_bins[0],time_bins[time_bins.length-1]]);
+
+  // Now put all data into bins...
+  var time_map = fill_binned_data(data_map,time_bins,bin_size);
+
+  var data_input = fill_data_input(time_bins,time_map,get_key_array(csv_map));
+  return data_input;
+}
+
+function plot_data(location,data_input,dose,data_labels,time,div) {
+  var title_text = location;
+  var y_text = dose;
+  // add x-label to beginning of data label array
+  data_labels.unshift("Time (local)");
+  if( time=="All" ) { title_text = 'All data for ' + title_text; }
 
   g = new Dygraph(
     // containing div
@@ -262,7 +259,7 @@ function plot_data(location,dose,time,div) {
       sigFigs: 3,
       ylabel: y_text,
       xlabel: 'Time (local)',
-      labels: [ "Time (local)", data_label],
+      labels: data_labels,
       strokeWidth: 0.0,
       highlightCircleSize: 5,
       plotter: [
@@ -282,21 +279,48 @@ function plot_data(location,dose,time,div) {
   );
 }
 
-function get_data(url,location,dose,time,div) {
-  $.get(url, function (data) {
-      data_input = []; // Clear any old data out before filling!
-      //var dataStr = new String(data);
-      //var dataStr = data;
-      data_input = process_csv(data,dose,time);
-      plot_data(location,dose,time,div);
-  },dataType='text');
+function add_data_string(data,location) {
+  data_string_map.set(location,data);
 }
 
-function get_all_data(url_array,location,dose,time,div) {
-  for (index = 0; index < url_array.length; index++) {
-    $.get(url_array[index], data_string_array.push(data), dataType='text');
-  }
-  data_input = []; // Clear any old data out before filling!
-  process_all_data(data_string_array,dose,time);
-  plot_data(location,dose,time,div);
+function process_urls(url_array,locations) {
+  var csv_get_done = [];
+  $.each(url_array,function(i,url) {
+    csv_get_done.push($.get(url, function(data) {
+      add_data_string(data,locations[i]);
+      console.log('adding data from '+url+' to '+locations[i]);
+    }, dataType='text'));
+  });
+  return csv_get_done;
+}
+
+function get_key_array(map) {
+  var key_array = [];
+  map.forEach( function(value, key, map) {
+    key_array.push(key);
+  });
+  return key_array;
+}
+
+function get_all_data(url_array,locations,dose,time,div) {
+  data_string_map.clear();
+
+  csv_get_done = process_urls(url_array,locations);
+  $.when.apply($, csv_get_done).then( function() {
+    var return_locations = get_key_array(data_string_map);
+    var data_input = [];
+    data_input = process_all_data(data_string_map,dose,time);
+    plot_data("All locations",data_input,dose,return_locations,time,div);
+  });
+}
+
+function get_data(url,location,dose,time,div) {
+  $.get(url, function (data) {
+      var data_input = []; // Clear any old data out before filling!
+      data_input = process_csv(data,dose,time);
+      var data_label = [];
+      if ( dose=="&microSv/hr" ) { data_label.push("µSv/hr"); }
+      else data_label.push(dose);
+      plot_data(location,data_input,dose,data_label,time,div);
+  },dataType='text');
 }
