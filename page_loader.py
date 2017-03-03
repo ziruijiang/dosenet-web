bfc49c6dc71737134b86d9b914cd4fb48a8cfca6
alihanks <alihanks@gmail.com>
2017-03-03 14:39:48 -0800

    updates to prepare for new display format

--- page_loader.py
+++ page_loader.py
@@ -3,17 +3,19 @@ from time import sleep
 
 if __name__ == "__main__":
     urls = ['file:///home/pi/dosenet-web/display-monitors/Front.html', 'https://radwatch.berkeley.edu',
-    		'file:///home/pi/dosenet-web/display-monitors/Rad101.html',
+            'https://radwatch.berkeley.edu/rad101#tab2',
+            'https://radwatch.berkeley.edu/rad101#tab3',
     		'https://radwatch.berkeley.edu/dosenet/map#dosenet_rad_banner',
     		'https://radwatch.berkeley.edu/dosenet/data#chartdata',
-    		'file:///home/pi/dosenet-web/display-monitors/Locations2.html',
+    		'https://radwatch.berkeley.edu/dosenet/data#chartdata',
     		'file:///home/pi/dosenet-web/display-monitors/WeatherStation.html',
             'file:///home/pi/dosenet-web/display-monitors/FindMore.html']
-    sleeps = [10,60,10,30,30,30,20,15]
+    sleeps = [10,30,10,10,30,30,30,20,15]
 b = webdriver.Firefox()
+b.set_window_position(0,0)
+b.set_window_size(1024, 768)
 
 while True:
     for idx, url in enumerate(urls):
-        b.maximize_window()
         b.get(url)
         sleep(sleeps[idx])
